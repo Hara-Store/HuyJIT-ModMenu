6ef60a86d0f7db6773fc251bb7956d3f7b00efe8
Hara_Store <haryadxalil25@gmail.com>
2026-04-03 22:58:31 +0300

    Update NakanoIchika.h

--- 5Toubun/NakanoIchika.h
+++ 5Toubun/NakanoIchika.h
@@ -1,6 +1,27 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <UIKit/UIAlertView.h>
-#import <UIKit/UIControl.h>
-//#include <JRMemory/MemScan.h>
-//Actually if you need JRMemory, just add it back
+
+// --- HARA_IOS MOD MENU CONFIGURATION ---
+#define MENU_TITLE @"HARA_IOS VIP"
+#define MENU_SUBTITLE @"PUBG MOBILE MOD"
+#define MENU_VERSION @"1.0.1"
+
+@interface HARA_Menu : UIView
+
+// Main Methods
++ (void)showMenu;
+- (void)setupMenuUI;
+- (void)handleSwitchChange:(UISwitch *)sender;
+
+@end
+
+// --- BOOLS FOR SWITCHES ---
+static bool isWallhackEnabled = false;
+static bool isNoRecoilEnabled = false;
+static bool isAntennaEnabled = false;
+static bool isSpeedHackEnabled = false;
+
+/* Note: This is the Header file (.h). 
+Next, we need to edit the Implementation file (.mm) 
+to add the actual PUBG Offsets.
+*
